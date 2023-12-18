88055ce9981fd63cd5ac53762f5330ffbf9b3a1d
Per Berge <per.berge93@gmail.com>
2023-12-18 12:02:28 +0100

    (feat): Modals for sign in/up/out created

---
+++ src/components/Auth/SignUp/SignUpModal.tsx
@@ -0,0 +1,58 @@
+import { Box } from "@mui/material";
+import Modal from "@mui/material/Modal";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+
+interface Props {
+  isModalOpen: boolean;
+}
+
+export const SignUpModal = (props: Props) => {
+  return (
+    <Modal
+      style={{
+        backdropFilter: "blur(4px)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      open={props.isModalOpen}
+    >
+      <Box
+        sx={{
+          position: "relative" as const,
+          width: "66%",
+          height: "80%",
+          bgcolor: "background.paper",
+          borderRadius: 2,
+        }}
+      >
+        <CloseRoundedIcon
+          sx={{
+            position: "absolute",
+            fontSize: 42,
+            right: 10,
+            top: 10,
+            ":hover": {
+              cursor: "pointer",
+            },
+          }}
+          color="disabled"
+          // onClick={() => props.setIsSignInOpen(false)}
+        />
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 4,
+          }}
+        >
+          SignFields here!
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
