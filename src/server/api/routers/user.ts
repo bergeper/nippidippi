882c25b462076a4a69c138bc6f73b60f76218e7a
Per Berge <per.berge93@gmail.com>
2023-12-20 18:35:35 +0100

    (fix): Error on build vercel

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -4,18 +4,18 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { db } from "~/server/db";
 
 export const userRouter = createTRPCRouter({
-  createUser: publicProcedure
-    .input(z.object({ name: z.string(), password: z.string() }))
-    .query(async ({ input }) => {
-      const newUser = await db.user.create({
-        data: {
-          username: input.name,
-          password: input.password,
-        },
-      });
-      // Add check if user exits in db
-      return {
-        user: newUser,
-      };
-    }),
+  // createUser: publicProcedure
+  //   .input(z.object({ name: z.string(), password: z.string() }))
+  //   .query(async ({ input }) => {
+  //     const newUser = await db.user.create({
+  //       data: {
+  //         username: input.name,
+  //         password: input.password,
+  //       },
+  //     });
+  //     // Add check if user exits in db
+  //     return {
+  //       user: newUser,
+  //     };
+  //   }),
 });
