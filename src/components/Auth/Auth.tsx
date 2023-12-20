94dd53b3b110cc16ac80138e0cb7c3f75c7ae704
Per Berge <per.berge93@gmail.com>
2023-12-20 18:18:26 +0100

    (feat): sign in/up/our buttons

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -4,10 +4,14 @@ import { useState } from "react";
 import { SignInModal } from "./SignIn/SignInModal";
 import { SignUpModal } from "./SignUp/SignUpModal";
 import { Button } from "@mui/material";
+import { useSession } from "next-auth/react";
+import { SignOutModal } from "./SignOut/SignOutModal";
 
 export const Auth = () => {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
   const [isSignInOpen, setIsSignInOpen] = useState(false);
+  const [isSignOutOpen, setIsSignOutOpen] = useState(false);
+  const { data: session } = useSession();
   return (
     <>
       <Button variant="outlined" onClick={() => setIsSignInOpen(true)}>
@@ -15,6 +19,9 @@ export const Auth = () => {
       </Button>
       <Button variant="contained" onClick={() => setIsSignUpOpen(true)}>
         SIGN UP
+      </Button>
+      <Button variant="contained" onClick={() => setIsSignOutOpen(true)}>
+        SIGN OUT
       </Button>
       <SignUpModal
         isModalOpen={isSignUpOpen}
@@ -24,6 +31,11 @@ export const Auth = () => {
         isModalOpen={isSignInOpen}
         setIsSignInOpen={setIsSignInOpen}
       />
+      <SignOutModal
+        isModalOpen={isSignOutOpen}
+        setIsSignOutOpen={setIsSignOutOpen}
+      />
+      {session && <p>{JSON.stringify(session)}</p>}
     </>
   );
 };
