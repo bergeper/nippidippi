8b4c1fe71805550cd6cbd0c364d886bc9ac8be48
Per Berge <per.berge93@gmail.com>
2023-12-20 18:18:07 +0100

    (refactor): removed unsused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { getServerAuthSession } from "~/app/api/auth/[...nextauth]/options";
 import { Auth } from "~/components/Auth/Auth";
 
 export default async function Home() {
-  const session = await getServerAuthSession();
-
   return (
     <>
       <Auth />
-      {session && <p>{session.id}</p>}
     </>
   );
 }
