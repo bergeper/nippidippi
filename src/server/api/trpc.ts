4be6a15ec8a039cd704c2454f17b19ce3b16903b
Per Berge <per.berge93@gmail.com>
2023-12-21 12:05:02 +0100

    (fix): cleaned unsused code for TRPC docs

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,44 +1,26 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import { type Session } from "next-auth";
-import { type NextRequest } from "next/server";
 import { getServerAuthSession } from "~/app/api/auth/[...nextauth]/options";
 import { db } from "../db";
 
 interface CreateContextOptions {
-  headers: Headers | null;
   session: Session | null;
-  ssg?: true;
 }
-
-/**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
- * it from here.
- *
- * Examples of things you may need it for:
- * - testing, so we don't have to mock Next.js' req/res
- * - tRPC's `createSSGHelpers`, where we don't have req/res
- *
- * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
- */
 export const createInnerTRPCContext = (opts: CreateContextOptions) => {
   return {
     session: opts.session,
-    headers: opts.headers,
-    ssg: opts.ssg,
     db,
   };
 };
 
 /**
- * This is the actual context you will use in your router. It will be used to process every request
- * that goes through your tRPC endpoint.
- *
+ * KEEP THIS DOC
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = async (opts: { req: NextRequest }) => {
+
+export const createTRPCContext = async () => {
   const session = await getServerAuthSession();
   return createInnerTRPCContext({
-    headers: opts.req.headers,
     session,
   });
 };
@@ -49,36 +31,26 @@ export const createTRPCContext = async (opts: { req: NextRequest }) => {
  */
 export const t = initTRPC.context<typeof createTRPCContext>().create();
 
-/*
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
+// Router
 export const router = t.router;
+
+// Middleware
 export const middleware = t.middleware;
+
+// Public Procedure to use on resolvers, anyone can access
 export const publicProcedure = t.procedure;
 
-/** Middleware for checking if user is admin */
+// Middleware for checking if the user is logged in
 const isLoggedIn = middleware(async (opts) => {
-  const { ctx } = opts;
-  if (!ctx.session || !ctx.session.user) {
+  if (!opts.ctx.session || !opts.ctx.session.user) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
   return opts.next({
     ctx: {
-      session: { ...ctx.session, user: ctx.session.user },
+      session: { ...opts.ctx.session, user: opts.ctx.session.user },
     },
   });
 });
 
-const isServer = middleware(async (opts) => {
-  const { ctx } = opts;
-  if (!ctx.ssg) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-
-  return opts.next();
-});
-
-/** Define new procedures that meet specific access criteria */
+// Procedure for resolvers when you are logged in.
 export const loggedInProcedure = t.procedure.use(isLoggedIn);
-export const serverProcedure = t.procedure.use(isServer);
