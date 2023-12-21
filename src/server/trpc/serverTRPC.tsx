e5a335759c732cfbd0703477046a978df73dd550
Per Berge <per.berge93@gmail.com>
2023-12-21 12:20:57 +0100

    (refactor): reminder to fix this later

--- src/server/trpc/serverTRPC.tsx
+++ src/server/trpc/serverTRPC.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { appRouter } from "../api/root";
 import { createInnerTRPCContext } from "../api/trpc";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const context = createInnerTRPCContext({
   session: null,
 });
 
 /** Should be used in RSC to generate pages with SSG */
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
 export const trpcCaller = appRouter.createCaller(context);
