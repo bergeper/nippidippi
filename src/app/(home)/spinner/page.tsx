42eb1e62d117735ecd439ec4fb573711e4bc3305
Per Berge <per.berge93@gmail.com>
2023-12-22 17:52:53 +0100

    (feat): basic layout for spinner page

--- src/app/(home)/spinner/page.tsx
+++ src/app/(home)/spinner/page.tsx
@@ -1,7 +1,44 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { theme } from "~/styles/theme/theme";
+
 export default function SpinnerPage() {
   return (
     <>
-      <h1>Spinner Pages</h1>
+      <Box
+        component="section"
+        sx={{
+          display: "flex",
+          width: "100%",
+          [theme.breakpoints.down("sm")]: {
+            flexDirection: "column",
+          },
+        }}
+      >
+        <Box
+          component="article"
+          sx={{
+            width: "50%",
+            [theme.breakpoints.down("sm")]: {
+              width: "100%",
+            },
+          }}
+        >
+          Grafic Man
+        </Box>
+        <Box
+          component="article"
+          sx={{
+            width: "50%",
+            [theme.breakpoints.down("sm")]: {
+              width: "100%",
+            },
+          }}
+        >
+          Link box
+        </Box>
+      </Box>
     </>
   );
 }
