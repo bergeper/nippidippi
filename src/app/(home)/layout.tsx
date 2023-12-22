94d259f761a19a8fcaf2909576c90e8ab84be628
Per Berge <per.berge93@gmail.com>
2023-12-22 18:04:19 +0100

    (feat): updated title

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -41,7 +41,7 @@ export default function HomeLayout({ children }: PropsWithChildren) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            ?
+            NippiDippi
           </Typography>
 
           <Box>
