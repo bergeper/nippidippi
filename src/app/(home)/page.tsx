f0a1ebf85f12b657d9f3d26a7ac60f55de348ae1
Per Berge <per.berge93@gmail.com>
2023-12-22 17:50:53 +0100

    (feat): layout for homepage created

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { theme } from "~/styles/theme/theme";
+
 export default function HomePage() {
-  return <></>;
+  return (
+    <>
+      <Box
+        component="section"
+        sx={{
+          display: "flex",
+          width: "100%",
+          [theme.breakpoints.down("sm")]: {
+            flexDirection: "column",
+          },
+        }}
+      >
+        <Box
+          component="article"
+          sx={{
+            width: "50%",
+            [theme.breakpoints.down("sm")]: {
+              width: "100%",
+            },
+          }}
+        >
+          Grafic Man
+        </Box>
+        <Box
+          component="article"
+          sx={{
+            width: "50%",
+            [theme.breakpoints.down("sm")]: {
+              width: "100%",
+            },
+          }}
+        >
+          Link box
+        </Box>
+      </Box>
+    </>
+  );
 }
