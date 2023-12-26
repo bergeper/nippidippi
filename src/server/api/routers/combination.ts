8e0841374188e6a42bf163d4d005187f28f1e507
Per Berge <per.berge93@gmail.com>
2023-12-26 10:58:59 +0100

    (feat): basic resolver for getting a combo based on a nr

--- src/server/api/routers/combination.ts
+++ src/server/api/routers/combination.ts
@@ -1,23 +1,19 @@
 import { db } from "~/server/db";
-import { router } from "../trpc";
+import { publicProcedure, router } from "../trpc";
 import { z } from "zod";
 
-const input = z.object({
-  name: z.string(),
-  flavor: z.string(),
+const getComboInput = z.object({
+  comboNr: z.string(),
 });
 
 export const combinationRouter = router({
-  // rateCombo: protectedProcedure.query(async ({ input }) => {
-  //   const newChip = await db.combination.create({
-  //     data: {
-  //       name: "Pers Umami",
-  //       chip: { connect: { id: "738a925a-aa7d-466a-878b-f3e3cef89f26" } },
-  //       dip: { connect: { id: "ce1c73c3-f18c-4954-8013-d489f584dca0" } },
-  //       rating: 0,
-  //     },
-  //     include: { chip: true, dip: true },
-  //   });
-  //   return newChip;
-  // }),
+  getCombo: publicProcedure.input(getComboInput).query(async ({ input }) => {
+    const combo = await db.combination.findUnique({
+      where: {
+        comboNr: input.comboNr,
+      },
+      include: { chip: true, dip: true },
+    });
+    return combo;
+  }),
 });
