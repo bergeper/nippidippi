02679973d3628694e1e5de212b0c01fce69ec02d
Per Berge <per.berge93@gmail.com>
2023-12-27 22:09:07 +0100

    (feat): basic styling

--- src/app/(web)/spinner/page.tsx
+++ src/app/(web)/spinner/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import { SaveResult } from "~/components/Spinner/SaveResult";
 import { SpinnerWheel } from "~/components/Spinner/SpinnerWheel";
 import { theme } from "~/styles/theme/theme";
+import NilpaIllu from "public/images/nilpa.png";
 
 export default function SpinnerPage() {
   return (
@@ -21,23 +24,35 @@ export default function SpinnerPage() {
           component="article"
           sx={{
             width: "50%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             [theme.breakpoints.down("sm")]: {
               width: "100%",
             },
           }}
         >
-          Grafic Man
+          <Typography variant="h4">Hello</Typography>
+          <Image
+            src={NilpaIllu}
+            width={300}
+            alt="Illustration of Nilpa the ChipGOD"
+          />
         </Box>
         <Box
           component="article"
           sx={{
             width: "50%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             [theme.breakpoints.down("sm")]: {
               width: "100%",
             },
           }}
         >
           <SpinnerWheel />
+          <SaveResult />
         </Box>
       </Box>
     </>
