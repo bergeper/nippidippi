0fa6066c177d5f3fa86db89359e647c1d9046a46
Per Berge <per.berge93@gmail.com>
2023-12-28 16:55:18 +0100

    (refactor): namechange on response from trpc

--- src/app/(web)/toplist/page.tsx
+++ src/app/(web)/toplist/page.tsx
@@ -3,12 +3,11 @@ import { TopList } from "~/components/Toplist/TopList";
 import { trpcApi } from "~/server/trpc/proxyTRPC";
 
 export default async function ToplistPage() {
-  const response = await trpcApi.combination.getTopCombos.query();
-  console.log(response);
+  const combos = await trpcApi.combination.getTopCombos.query();
   return (
     <Box>
       <Typography variant="h4">Nilpa TopList</Typography>
-      {response.map((c, i) => (
+      {combos.map((c, i) => (
         <Box key={i} sx={{ backgroundColor: "whitesmoke", p: 2, m: 2 }}>
           <TopList combo={c} />
         </Box>
