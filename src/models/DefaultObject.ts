1eec0219a99e1550cc3732384abc5a4cc36ea4a8
Per Berge <per.berge93@gmail.com>
2023-12-28 16:55:33 +0100

    (feat): defualt combo-object

---
+++ src/models/DefaultObject.ts
@@ -0,0 +1,20 @@
+export const defaultCombo = {
+  id: "",
+  comboNr: "",
+  name: "",
+  chipId: "",
+  dipId: "",
+  rating: 0,
+  chip: {
+    id: "",
+    name: "",
+    imgUrl: "",
+    flavor: "",
+  },
+  dip: {
+    id: "",
+    name: "",
+    imgUrl: "",
+    flavor: "",
+  },
+};
