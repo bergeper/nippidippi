40ba521209610cb9dfc870425e77ce61581e19f5
Per Berge <per.berge93@gmail.com>
2023-12-28 16:55:46 +0100

    (feat): props for a combo

--- src/components/Toplist/TopList.tsx
+++ src/components/Toplist/TopList.tsx
@@ -4,23 +4,46 @@ import { Box, Typography } from "@mui/material";
 import { useState } from "react";
 import { CombinationModal } from "./CombinationModal";
 import { type ICombination } from "~/models/ICombination";
+import { trpcApi } from "~/server/trpc/proxyTRPC";
+import { defaultCombo } from "~/models/DefaultObject";
 
 interface Props {
-  combo: ICombination;
+  combo: {
+    id: string;
+    comboNr: string;
+    name: string;
+    chipId: string;
+    dipId: string;
+    rating: number;
+  };
 }
 
 export const TopList = (props: Props) => {
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [combo, setCombo] = useState<ICombination>(defaultCombo);
+
+  const dataForModal = async (comboNr: string) => {
+    const response = await trpcApi.combination.getCombo.query({
+      comboNr: comboNr,
+    });
+    console.log("2");
+    if (response) {
+      setCombo(response);
+      setOpenModal(!openModal);
+    } else {
+      console.log("error");
+    }
+  };
 
   return (
     <>
-      <Box onClick={() => setOpenModal(!openModal)}>
+      <Box onClick={() => dataForModal(props.combo.comboNr)}>
         <Typography>{props.combo.name}</Typography>
         <Typography>{props.combo.comboNr}</Typography>
         <Typography>{props.combo.rating}</Typography>
       </Box>
       <CombinationModal
-        combo={props.combo}
+        combo={combo}
         isModalOpen={openModal}
         setIsModalOpen={setOpenModal}
       />
