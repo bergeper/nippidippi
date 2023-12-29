cea034be3b472f413b9f7532a7ba1af0553e3091
Per Berge <per.berge93@gmail.com>
2023-12-29 14:22:04 +0100

    (feat): list is printed by order.

--- src/app/(web)/toplist/page.tsx
+++ src/app/(web)/toplist/page.tsx
@@ -5,10 +5,11 @@ import { trpcCaller } from "~/server/trpc/serverTRPC";
 export default async function ToplistPage() {
   const combos = await trpcCaller.combination.getTopCombos();
   if (combos) {
+    const sortedCombos = combos.slice().sort((a, b) => b.rating - a.rating);
     return (
       <Box component="section">
         <Typography variant="h4">Nilpa TopList</Typography>
-        {combos.map((c, i) => (
+        {sortedCombos.map((c, i) => (
           <Box key={i} sx={{ backgroundColor: "whitesmoke", p: 2, m: 2 }}>
             <TopList combo={c} />
           </Box>
@@ -16,6 +17,6 @@ export default async function ToplistPage() {
       </Box>
     );
   } else {
-    return <>ERROR</>;
+    return <Typography variant="h1">ERROR</Typography>;
   }
 }
