f230fb153e91b62892444ba4c35251ee6c8b9f20
Per Berge <per.berge93@gmail.com>
2024-01-02 22:43:52 +0100

    (feat): will not save if combo is already saved

--- src/server/api/routers/combination.ts
+++ src/server/api/routers/combination.ts
@@ -38,13 +38,26 @@ export const combinationRouter = router({
   }),
   saveCombo: loggedInProcedure.input(comboId).mutation(async (opts) => {
     const { ctx, input } = opts;
-    const saveCombo = await ctx.db.triedCombination.create({
-      data: {
-        user: { connect: { id: ctx.session.user.id } },
-        combination: { connect: { id: input.comboId } },
+    const existingCombo = await ctx.db.triedCombination.findFirst({
+      where: {
+        user: { id: ctx.session.user.id },
+        combination: { id: input.comboId },
       },
     });
-    return saveCombo;
+
+    if (!existingCombo) {
+      const saveCombo = await ctx.db.triedCombination.create({
+        data: {
+          user: { connect: { id: ctx.session.user.id } },
+          combination: { connect: { id: input.comboId } },
+        },
+      });
+      return saveCombo;
+    } else {
+      console.log("combo already saved ❤️❤️❤️❤️❤️❤️🚀🚀🚀🚀⚠️⚠️⚠️⚠️");
+      // send back better request
+      return existingCombo;
+    }
   }),
   getTopCombos: publicProcedure.query(async () => {
     const combos = await db.combination.findMany({
@@ -68,12 +81,35 @@ export const combinationRouter = router({
       const { ctx, input } = opts;
       console.log(opts.ctx.session?.user.id);
       const testedCombos = await ctx.db.triedCombination.findMany({
-        where: { userId: input.userId },
+        where: { user: { id: input.userId } },
         include: {
           combination: true,
         },
       });
 
       return testedCombos;
+    }),
+  rateCombo: loggedInProcedure
+    .input(
+      z.object({
+        rating: z.number(),
+        comboId: z.string(),
+      })
+    )
+    .query(async (opts) => {
+      const { ctx, input } = opts;
+      const comboToRate = await db.combination.findUnique({
+        where: { id: input.comboId },
+      });
+
+      if (!comboToRate) {
+        throw new Error("Combination not found");
+      }
+
+      // const newating =
+      //   (comboToRate.rating * comboToRate.ratings.length + input.rating) /
+      //   (comboToRate.ratings.length + 1);
+      // I want to take comboToRates number and add new rating.
+      return;
     }),
 });
