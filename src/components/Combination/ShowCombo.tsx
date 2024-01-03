232fe23033b04d0ae8e997c461697920382f2d6f
Per Berge <per.berge93@gmail.com>
2024-01-03 15:06:23 +0100

    (feat): will display spinned result

---
+++ src/components/Combination/ShowCombo.tsx
@@ -0,0 +1,37 @@
+import { Box, Typography } from "@mui/material";
+import { type ICombination } from "~/models/ICombination";
+import Image from "next/image";
+
+interface Props {
+  combo: ICombination;
+}
+
+export const ShowCombo = ({ combo }: Props) => {
+  return (
+    <>
+      <Typography>{combo.name}</Typography>
+      <Box sx={{ width: "100%", display: "flex", alignItems: "center" }}>
+        <Box sx={{ width: "50%" }}>
+          <Image
+            src={combo.chip.imgUrl}
+            width={125}
+            height={166.66}
+            alt="test"
+          />
+          <Typography>{combo.chip.name}</Typography>
+          <Typography>{combo.chip.flavor}</Typography>
+        </Box>
+        <Box sx={{ width: "50%" }}>
+          <Image
+            src={combo.dip.imgUrl}
+            width={125}
+            height={166.66}
+            alt="test"
+          />
+          <Typography>{combo.dip.name}</Typography>
+          <Typography>{combo.dip.flavor}</Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
