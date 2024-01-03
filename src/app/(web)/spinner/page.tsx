9977d3f85b04234b18e221e62759f2a9c4aedd6b
Per Berge <per.berge93@gmail.com>
2024-01-03 15:06:38 +0100

    (refactor): small styling fixes

--- src/app/(web)/spinner/page.tsx
+++ src/app/(web)/spinner/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Box } from "@mui/material";
-import { SaveResult } from "~/components/Spinner/SaveResult";
 import { SpinnerWheel } from "~/components/Spinner/SpinnerWheel";
 import { theme } from "~/styles/theme/theme";
 
@@ -21,7 +20,7 @@ export default function SpinnerPage() {
         <Box
           component="article"
           sx={{
-            width: "50%",
+            width: "100%",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
@@ -31,7 +30,6 @@ export default function SpinnerPage() {
           }}
         >
           <SpinnerWheel />
-          <SaveResult />
         </Box>
       </Box>
     </>
