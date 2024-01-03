dbb29072ac83d24e2fc36decb97871edb5535956
Per Berge <per.berge93@gmail.com>
2024-01-03 18:16:43 +0100

    (feat): rating will be displayed

--- src/components/Combination/CombinationList.tsx
+++ src/components/Combination/CombinationList.tsx
@@ -6,6 +6,8 @@ import { CombinationModal } from "./CombinationModal";
 import { type ICombination } from "~/models/ICombination";
 import { trpcApi } from "~/server/trpc/proxyTRPC";
 import { defaultCombo } from "~/models/DefaultObject";
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
 
 interface Props {
   combo: {
@@ -41,6 +43,15 @@ export const CombinationList = (props: Props) => {
           Combo Number: {props.combo.comboNr}
         </Typography>
         <Typography variant="h6">Rating: {props.combo.rating}</Typography>
+        <Stack spacing={1}>
+          {/* <Rating name="half-rating" defaultValue={2.5} precision={0.5} /> */}
+          <Rating
+            name="half-rating-read"
+            defaultValue={props.combo.rating}
+            precision={0.5}
+            readOnly
+          />
+        </Stack>
       </Box>
       <CombinationModal
         combo={combo}
