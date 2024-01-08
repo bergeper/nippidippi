289235040e80ad4510a1a4463fdf9b677900f81f
Per Berge <per.berge93@gmail.com>
2024-01-08 10:24:01 +0100

    (feat): rouletteWheelFrame Addded

--- src/components/Spinner/SpinnerWheel.tsx
+++ src/components/Spinner/SpinnerWheel.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Button, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
 import { type ICombination } from "~/models/ICombination";
 import { trpcApi } from "~/server/trpc/proxyTRPC";
 import RouletteWheel from "public/images/roulette-wheel.png";
+import WheelWithArrow from "public/images/WheelWithArrow2.png";
 import { ShowCombo } from "../Combination/ShowCombo";
 
 export const SpinnerWheel = () => {
@@ -50,13 +51,20 @@ export const SpinnerWheel = () => {
       <Typography variant="h4" sx={{ fontSize: "1.5rem" }}>
         Spin the wheel and see what you get!
       </Typography>
-      <Image
-        src={RouletteWheel}
-        width={250}
-        alt="Spinner wheel"
-        onClick={getCombo}
-      />
-
+      <Box sx={{ display: "flex", position: "relative" }}>
+        <Image
+          src={WheelWithArrow}
+          width={400}
+          alt="Spinner Wheel Frame"
+          style={{ position: "absolute" }}
+        />
+        <Image
+          src={RouletteWheel}
+          width={400}
+          alt="Spinner wheel"
+          onClick={getCombo}
+        />
+      </Box>
       {combo && <ShowCombo combo={combo} />}
       {error}
       {session && combo && (
