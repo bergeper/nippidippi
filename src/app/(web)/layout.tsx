92577c1f8d9e66710b9a9954f8e9c0b2819a1d85
Per Berge <per.berge93@gmail.com>
2024-01-09 12:43:38 +0100

    (feat): bg-image

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -6,6 +6,7 @@ import {
   IconButton,
   Menu,
   MenuItem,
+  MenuList,
   Toolbar,
   Typography,
 } from "@mui/material";
@@ -13,20 +14,20 @@ import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import React, { useState } from "react";
 import { type PropsWithChildren } from "react";
-// import { Auth } from "~/components/Auth/Auth";
 import { useSession } from "next-auth/react";
 import { NavMenu } from "~/components/Menu/NavMenu";
-import { theme } from "~/styles/theme/theme";
 import Link from "next/link";
 import rainingChips from "public/images/rainingChips.jpg";
+import { theme } from "~/styles/theme/theme";
 
 export default function HomeLayout({ children }: PropsWithChildren) {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [isMainMenuOpen, setIsMainMenuOpen] = useState(false);
 
   const { data: session, status } = useSession();
-  const ImgOverlay =
-    "linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3))";
+  const ImgOverlay = "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3))";
+  // const ImgOverlay =
+  //   "linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3))";
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -37,7 +38,7 @@ export default function HomeLayout({ children }: PropsWithChildren) {
   };
   return (
     <>
-      <AppBar position="static">
+      <AppBar color="inherit" position="static" sx={{ background: " #EEE3AC" }}>
         <Toolbar>
           <IconButton
             color="inherit"
@@ -82,22 +83,35 @@ export default function HomeLayout({ children }: PropsWithChildren) {
               onClose={handleClose}
             >
               {status === "authenticated" && (
-                <>
-                  <MenuItem href="/my-pages" onClick={handleClose}>
-                    My Pages
-                  </MenuItem>
-                  <MenuItem onClick={handleClose} href="/my-pages/settings">
-                    Settings
-                  </MenuItem>
-                  <MenuItem onClick={handleClose} href="/my-pages/results">
-                    Results
-                  </MenuItem>
-                </>
+                <MenuList>
+                  <Link id="my-pages" color="textSecondary" href="/my-pages">
+                    <MenuItem href="/my-pages" onClick={handleClose}>
+                      My Pages
+                    </MenuItem>
+                  </Link>
+                  <Link
+                    id="settings"
+                    color="textSecondary"
+                    href="/my-pages/settings"
+                  >
+                    <MenuItem onClick={handleClose} href="/my-pages/settings">
+                      Settings
+                    </MenuItem>
+                  </Link>
+                  <Link
+                    id="results"
+                    color="textSecondary"
+                    href="/my-pages/results"
+                  >
+                    <MenuItem onClick={handleClose} href="/my-pages/results">
+                      Results
+                    </MenuItem>
+                  </Link>
+                </MenuList>
               )}
               <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
                 {session ? "Sign out" : "Sign in"}
               </Link>
-              {/* {status === "unauthenticated" && <Auth />} */}
             </Menu>
           </Box>
         </Toolbar>
@@ -109,13 +123,17 @@ export default function HomeLayout({ children }: PropsWithChildren) {
       <Box
         component="main"
         sx={{
-          minHeight: "100vh",
           width: "auto",
-          // background: theme.palette.primary.dark,
+          minHeight: "100vh",
           backgroundImage: `${ImgOverlay}, url(${rainingChips.src})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
+          [theme.breakpoints.down("sm")]: {
+            minHeight: "auto",
+            flexDirection: "column-reverse",
+            justifyContent: "space-between",
+          },
         }}
       >
         {children}
@@ -124,8 +142,9 @@ export default function HomeLayout({ children }: PropsWithChildren) {
         component="footer"
         sx={{
           height: "40px",
-          backgroundColor: "black",
-          bottom: 0,
+          width: "100%",
+          background: "#EEE3AC",
+          b: 0,
         }}
       >
         Footen
