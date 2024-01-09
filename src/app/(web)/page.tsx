bdd02e04b77517744625fe6128b020b36dafd562
Per Berge <per.berge93@gmail.com>
2024-01-09 14:12:23 +0100

    (feat): desktop styling done

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -5,6 +5,8 @@ import { theme } from "~/styles/theme/theme";
 import NilpaIllu from "public/images/nippi.png";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { homepageCopy } from "~/texts/texts";
+import { LinkBox } from "~/components/StartPage/LinkBox";
 
 export default function HomePage() {
   const mediumWindow = useMediaQuery(theme.breakpoints.up("sm"));
@@ -25,26 +27,36 @@ export default function HomePage() {
       sx={{
         display: "flex",
         width: "100%",
-        height: "auto",
-        [theme.breakpoints.down("sm")]: {
-          height: "660px",
-          flexDirection: "column-reverse",
-          justifyContent: "space-between",
+        height: "660px",
+        flexDirection: "column-reverse",
+        justifyContent: "space-between",
+        [theme.breakpoints.up("sm")]: {
+          height: "100%",
+          width: "90%",
+          p: 1,
+          flexDirection: "row",
+          flexWrap: "wrap",
+          mt: 20,
+        },
+        [theme.breakpoints.up("lg")]: {
+          width: "1000px",
         },
       }}
     >
       <Box
         component="article"
         sx={{
-          width: "50%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          p: 2,
-          [theme.breakpoints.down("sm")]: {
-            width: "100%",
-            mb: -0.5,
-            p: 0,
+          width: "100%",
+          mb: -0.5,
+          p: 0,
+
+          [theme.breakpoints.up("sm")]: {
+            width: "50%",
+            p: 2,
+            pb: 0,
           },
         }}
       >
@@ -65,6 +77,10 @@ export default function HomePage() {
               background: "white",
               borderRadius: "25px", // Adjust the value as needed
               fontSize: "1rem",
+              [theme.breakpoints.up("md")]: {
+                right: 40,
+                top: 100,
+              },
             }}
             href="/spinner"
           >
@@ -81,31 +97,40 @@ export default function HomePage() {
       <Box
         component="article"
         sx={{
-          width: "50%",
+          width: "auto",
           display: "flex",
           flexDirection: "column",
-          background:
-            "linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6))",
+          background: theme.palette.custom.custom,
           p: 2,
-          height: "auto",
-          [theme.breakpoints.down("sm")]: {
-            width: "auto",
+
+          [theme.breakpoints.up("sm")]: {
+            height: "auto",
+            alignContent: "center",
+            justifyContent: "center",
+            width: "40%",
+            borderRadius: "4px",
           },
+          // RGB(238, 227, 172)
         }}
       >
         <Typography
           color="inherit"
           variant="h1"
-          sx={{ fontSize: "1.3rem", textAlign: "center", pt: 2, pb: 2 }}
+          sx={{ fontSize: "1.34rem", textAlign: "center", pt: 2, pb: 2 }}
         >
           Welcome to NippiDippi
         </Typography>
         <Typography color="inherit" variant="h1" sx={{ fontSize: "1.225rem" }}>
-          Where our Chip Wheel guides you to the perfect chip and dip pairing.
-          Elevate your snacking experience with our crispy delights and
-          flavorful dips – the ultimate match for your taste buds!
+          Welcome to Chip and Dip Roulette, where flavor meets chance! Meet
+          "Nippi," the culinary genius crafting unforgettable chip and dip
+          combos. For the indecisive snacker, Nippi curates solid choices,
+          removing the guesswork from your dilemma.
+          <br />
+          <br />
+          {largeWindow && homepageCopy}
         </Typography>
       </Box>
+      {mediumWindow && <LinkBox />}
     </Box>
   );
 }
