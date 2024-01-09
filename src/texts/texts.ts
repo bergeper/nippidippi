42eb8e923fe866190c562a80631063f5fad2a7e9
Per Berge <per.berge93@gmail.com>
2024-01-09 14:11:20 +0100

    (feat): copy for startpage

---
+++ src/texts/texts.ts
@@ -0,0 +1,8 @@
+export const homepageCopy = ` Dive into the world of flavor with confidence, as Nippi's
+  combinations are designed to tantalize your taste buds and elevate
+  your chip and dip game. Unsure of where to begin? Fear not, for
+  Nippi has you covered. Simply log in through Discord, embark on a
+  flavorful journey, and rate your results to help others discover
+their perfect match. Chip and Dip Roulette is not just a game it's
+  a culinary adventure that transforms indecision into delectable
+delight. Join us in savoring the unexpected, one dip at a time!`;
