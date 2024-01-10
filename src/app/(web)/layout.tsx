883493c596a98a2c4883115cb7ed62ad2bf405b6
Per Berge <per.berge93@gmail.com>
2024-01-10 15:28:23 +0100

    (feat): account menu styled. keeping white.

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -85,33 +85,50 @@ export default function HomeLayout({ children }: PropsWithChildren) {
             >
               {status === "authenticated" && (
                 <MenuList>
-                  <Link id="my-pages" color="textSecondary" href="/my-pages">
-                    <MenuItem href="/my-pages" onClick={handleClose}>
+                  <Link
+                    id="my-pages"
+                    href="/my-pages"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <MenuItem
+                      href="/my-pages"
+                      onClick={handleClose}
+                      sx={{ color: "black" }}
+                    >
                       My Pages
                     </MenuItem>
                   </Link>
                   <Link
                     id="settings"
-                    color="textSecondary"
+                    style={{ textDecoration: "none" }}
                     href="/my-pages/settings"
                   >
-                    <MenuItem onClick={handleClose} href="/my-pages/settings">
+                    <MenuItem onClick={handleClose} sx={{ color: "black" }}>
                       Settings
                     </MenuItem>
                   </Link>
                   <Link
                     id="results"
-                    color="textSecondary"
                     href="/my-pages/results"
+                    style={{ textDecoration: "none" }}
                   >
-                    <MenuItem onClick={handleClose} href="/my-pages/results">
+                    <MenuItem
+                      onClick={handleClose}
+                      href="/my-pages/results"
+                      sx={{ color: "black" }}
+                    >
                       Results
                     </MenuItem>
                   </Link>
                 </MenuList>
               )}
-              <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
-                {session ? "Sign out" : "Sign in"}
+              <Link
+                href={session ? "/api/auth/signout" : "/api/auth/signin"}
+                style={{ textDecoration: "none" }}
+              >
+                <MenuItem sx={{ color: "black" }}>
+                  {session ? "Sign out" : "Sign in"}
+                </MenuItem>
               </Link>
             </Menu>
           </Box>
