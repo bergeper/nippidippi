9bdd2f0c44439b90ef5c0981d29c3c72802a5c2e
Per Berge <per.berge93@gmail.com>
2024-01-11 11:42:05 +0100

    (refactor): moved box to ToplistComponent so this file can stay SSR

--- src/app/(web)/toplist/page.tsx
+++ src/app/(web)/toplist/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Toplist } from "~/components/Toplist/Toplist";
 import { ToplistTitle } from "~/components/Toplist/ToplistTitle";
 import { trpcCaller } from "~/server/trpc/serverTRPC";
@@ -7,19 +7,10 @@ export default async function ToplistPage() {
   const combos = await trpcCaller.combination.getTopCombos();
   if (combos) {
     return (
-      <Box
-        component="section"
-        sx={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+      <>
         <ToplistTitle />
         <Toplist combos={combos} />
-      </Box>
+      </>
     );
   } else {
     return <Typography variant="h1">ERROR</Typography>;
